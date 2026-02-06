a5d033b9000fc2a653056fc993a2075d22621572
cafeed28 <62172116+cafeed28@users.noreply.github.com>
2026-02-06 23:39:37 +0500

    partner depot: CWeaponCSBase::Recoil

--- src/game/shared/cstrike15/weapon_csbase.cpp
+++ src/game/shared/cstrike15/weapon_csbase.cpp
@@ -3832,7 +3832,19 @@ void CWeaponCSBase::OnLand( float fVelocity )
 
 void CWeaponCSBase::Recoil( CSWeaponMode weaponMode )
 {
-	/** Removed for partner depot **/
+	CCSPlayer* pPlayer = GetPlayerOwner();
+	if ( !pPlayer )
+		return;
+
+	int iIndex = IsFullAuto() ? m_flRecoilIndex : GetPredictionRandomSeed();
+
+	float fAngle, fMagnitude;
+	if ( weapon_legacy_recoiltable.GetBool() )
+		GetCSWpnData().GetRecoilOffsets( weaponMode, iIndex, fAngle, fMagnitude );
+	else
+		g_WeaponRecoilData.GetRecoilOffsets( this, weaponMode, iIndex, fAngle, fMagnitude );
+
+	pPlayer->KickBack( fAngle, fMagnitude );
 }
 
 #ifdef CLIENT_DLL
