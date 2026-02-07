7b30086eaa3c68f8d54df0d46cf57b8b631d81bc
cafeed28 <62172116+cafeed28@users.noreply.github.com>
2026-02-07 20:35:20 +0500

    gcsdk: SpewType_t re note: based on portal2 client.so.dbg dwarfdump DW_AT_name	("SpewType_t") DW_AT_decl_file	("<snip>igamecoordinatorhost.h") DW_AT_decl_line	(17)

--- src/gcsdk/steamextra/gamecoordinator/igamecoordinatorhost.h
+++ src/gcsdk/steamextra/gamecoordinator/igamecoordinatorhost.h
@@ -14,6 +14,17 @@
 class CSteamID;
 class IGCSQLQuery;
 
+enum SpewType_t
+{
+	SPEW_MESSAGE = 0,
+	SPEW_WARNING,
+	SPEW_ASSERT,
+	SPEW_ERROR,
+	SPEW_LOG,
+
+	SPEW_TYPE_COUNT
+};
+
 class IGameCoordinatorHost
 {
 public:
